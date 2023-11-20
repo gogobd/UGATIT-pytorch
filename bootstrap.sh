64facebbda96d182e8ef68918e9eb3ff680f1a05
Georg Bernhard <gogo@bluedynamics.com>
2023-11-20 21:51:49 +0000

    selfie2anime

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 conda init
-source /app/.bashrc
+source /root/.bashrc
 conda install pytorch-cuda=12.1 torchvision pillow scipy opencv -c conda-forge  -c anaconda -c pytorch -c nvidia
 # apt-get install ffmpeg libsm6 libxext6
-# ln -s /app/data/datasets/selfie2anime dataset/selfie2anime
+# ln -s /app/data/datasets/selfie2anime /app/dataset/selfie2anime
 # python main.py --dataset selfie2anime --light=True
